b604277a33332de9d2569f7a5b7599b56c0842ea
neo <defneorise@gmail.com>
2025-03-03 14:35:10 -0500

    feat: add main structure to main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,33 @@
 
 export default function Home() {
   return (
-      <>
-        <h1> Intercer Peru </h1>
-      </>
+      <div>
+          <section>
+              <h1> Intercert Peru </h1>
+          </section>
+          <section>
+              {/*TODO: add */}
+          </section>
+          <section>
+              <h2> Servicios </h2>
+          </section>
+          <section>
+              <h2> Acreditaciones </h2>
+          </section>
+          <section>
+              {/*TODO: add years in buissness*/}
+          </section>
+          <section>
+              <h2> Parthners </h2>
+          </section>
+          <section>
+              <h2> Porque intercert  </h2>
+          </section>
+          <section>
+              <h2> Contacto </h2>
+          </section>
+
+
+      </div>
   );
 }
