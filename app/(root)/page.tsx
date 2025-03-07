629575aca276ca5264c56cb904a4703194b84dbf
neo <defneorise@gmail.com>
2025-03-07 14:33:05 -0500

    feat: add banner to main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,24 +3,31 @@
 import Link from "next/link"
 import Image from "next/image"
 import { Button } from "@/components/ui/button"
+import bg from '../../public/certificate-background.png'
 
 export default async function Home() {
   return (
       <>
-          <section id="presentation" className="w-full py-12 md:py-24 lg:py-32 bg-muted">
-              <div className="container px-4 md:px-6">
+          <section id="presentation" className="w-full py-12 md:py-24 lg:py-32 bg-muted"
+                   style={{
+                       backgroundImage: `url(${bg.src})`,
+                       width: '100%',
+                       objectFit: 'contain',
+                   }}>
+              <div className="container px-4 md:px-6" >
                   <div className="grid gap-6 lg:grid-cols-2 lg:gap-12 items-center">
-
                       <div className="space-y-4">
                           <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-                              Your Trusted Partner for ISO Certification in Peru
+                              Especialistas en Certificaciones ISO de alta calidad
                           </h1>
                           <p className="max-w-[600px] text-muted-foreground md:text-xl">
-                              Intercert helps organizations achieve international standards through comprehensive ISO certification
-                              services. Enhance your credibility and operational excellence.
+                              Con años de experiencia y un equipo altamente calificado, garantizamos que tu empresa cumpla
+                              con los estándares internacionales de calidad.
                           </p>
                           <div className="flex flex-col gap-2 min-[400px]:flex-row">
-                              <Button size="lg">Get Started</Button>
+                              <Link href='/contacto' passHref>
+                                  <Button size="lg"> Contáctanos </Button>
+                              </Link>
                               <Button variant="outline" size="lg">
                                   Learn More
                               </Button>
