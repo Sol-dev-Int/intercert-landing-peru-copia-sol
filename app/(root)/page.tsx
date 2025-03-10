cf80de320084afe57d751435a8c93de11e813bbb
neo <defneorise@gmail.com>
2025-03-09 22:03:33 -0500

    fix: rm unused Image import

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,6 @@
 'use server'
 
 import Link from "next/link"
-import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import bg from '../../public/certificate-background.png'
 
