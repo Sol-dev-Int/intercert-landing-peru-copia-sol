94e9d4e2c8c60da5674cd55bd894e2e8c2fe9563
neo <defneorise@gmail.com>
2025-03-09 21:56:41 -0500

    feat: add 404 error page

---
+++ app/not-found.tsx
@@ -0,0 +1,22 @@
+'use server'
+
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+
+export default async function IntercertNotFound() {
+    return (
+        <div className={`h-dvh flex flex-col justify-center items-center px-12 text-center gap-4`}>
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                Error 404 - Pagina No Encontrada
+            </h1>
+            <p className="leading-7 [&:not(:first-child)]:mt-6">
+                Parece que la Pagina que buscas no existe :c
+            </p>
+            <Button size="lg" asChild>
+                <Link href='/'>
+                    Regresar al Inicio
+                </Link>
+            </Button>
+        </div>
+    );
+}
